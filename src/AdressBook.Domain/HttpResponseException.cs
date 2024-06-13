c514a69d322f180750c9125667462f28c19ef30e
Dominik <dominik.podraza@adminbrothers.com>
2024-06-13 13:31:20 +0200

    Rebase of exceptions

--- src/AdressBook.Domain/HttpResponseException.cs
+++ src/AdressBook.Domain/HttpResponseException.cs
@@ -1,6 +1,6 @@
 ﻿using System.Net;
 
-namespace AdressBook.Infrastructure.Middleware.Exceptions
+namespace AdressBook.Domain
 {
     public record ExceptionMessage(string Title, string Description);
 
